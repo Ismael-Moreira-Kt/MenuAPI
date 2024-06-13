2ab8a244c05e602ceb80fefe157ae28db5201272
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-13 02:55:18 +0100

    feat: Addition of the post method to add items to the menu.

--- src/main/java/com/example/menu/controllers/FoodController.java
+++ src/main/java/com/example/menu/controllers/FoodController.java
@@ -1,12 +1,12 @@
 package com.example.menu.controllers;
 
+import com.example.menu.dtos.FoodRequestDTO;
 import com.example.menu.dtos.FoodResponseDTO;
 import com.example.menu.entities.FoodEntity;
 import com.example.menu.repositories.FoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 
@@ -20,5 +20,12 @@ public class FoodController {
     @GetMapping
     public List<FoodResponseDTO> getAllItems() {
         return foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping("/addItem")
+    public void postItem(@RequestBody FoodRequestDTO data) {
+        FoodEntity foodData = new FoodEntity(data);
+        foodRepository.save(foodData);
     }
 }
